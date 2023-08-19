4d09f15cad5dd95b175ddb9ea9f618d6c0652e64
krishnachaturvedi1901 <krishna.chaturvedi1901@gmail.com>
2023-08-19 17:44:21 +0530

    first taileind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" text-3xl font-bold font-serif underline border-2 border-slate-700 p-4 text-center ">
+        Hello
+      </div>
+      <h3 className=" text-4xl text-center text-red-800" >Introduction</h3>
+      <h4>Hlw iam krishna </h4>
+      <h4></h4>
+    </>
+  );
+}
+
+export default App;
